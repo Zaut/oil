2e936edef3852ae528ee757b4c21c93425f104c1
Kisil Yurii <kisluy51@gmail.com>
2026-03-18 03:20:35 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Oil PWA',
+        short_name: 'Oil',
+        description: 'Oil progressive web app',
+        theme_color: '#111111',
+        background_color: '#111111',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+})
