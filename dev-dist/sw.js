2dee8a0be14ca612cdcaf397baff6fdd7fc5c06e
Kisil Yurii <kisluy51@gmail.com>
2026-03-19 20:45:39 +0300

    fix buttom carousel

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7sv036v97fs"
+    "revision": "0.8r1871q6rl4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
